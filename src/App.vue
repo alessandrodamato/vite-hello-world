de9fed6f4ed0cb28db946eb74d6e1937789e3fde
alessandrodamato <alessandro042004@gmail.com>
2024-03-20 13:31:03 +0100

    [edit] readme, [add] h1, p and styles

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,43 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default {
+  data(){
+    return{
+      title: 'Vite Hello World!',
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis quasi cupiditate sequi officiis quae officia repellendus veritatis sed! Aliquam minus deleniti dicta officia molestiae saepe accusantium natus enim beatae aspernatur.'
+    }
+  }
+}
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+
+    <header></header>
+
+    <main>
+
+      <h1>{{ title }}</h1>
+
+      <p>{{ text }}</p>
+
+    </main>
+
+    <footer></footer>
+
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
+<!-- stili specifici di app.vue -->
+<style>
+  
+  h1, p{
+    font-size: 3rem;
+    padding: 50px 0;
+  }
+
+  p{
+    font-size: 1.3rem;
+  }
+
 </style>
